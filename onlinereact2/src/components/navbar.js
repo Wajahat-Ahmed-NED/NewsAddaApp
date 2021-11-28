d3aac887f5cdfae463f70ea809acbce9441c08da
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-11-28 13:44:45 +0500

    update

--- onlinereact2/src/components/navbar.js
+++ onlinereact2/src/components/navbar.js
@@ -26,16 +26,15 @@ export default class Navbar extends Component {
                                
                                 
                                 <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                    <Link className="nav-link " aria-current="page" to="/">Home</Link>
                                 </li>
                                
                                 <li className="nav-item"><Link  className="nav-link" to="/business"> Business</Link></li>
-                                <li className="nav-item"><Link key={"entertainment"} className="nav-link" to="/entertainment"> Entertainment</Link></li>
-                                <li className="nav-item"><Link key={"general"} className="nav-link" to="/general"> General</Link></li>
-                                <li className="nav-item"><Link key={"health"} className="nav-link" to="/health"> Health</Link></li>
-                                <li className="nav-item"><Link key={"science"} className="nav-link" to="/science"> Science</Link></li>
-                                <li className="nav-item"><Link key={"sports"} className="nav-link" to="/sports"> Sports</Link></li>
-                                <li className="nav-item"><Link key={"technology"} className="nav-link" to="/technology"> Technology</Link></li>
+                                <li className="nav-item"><Link className="nav-link " key={"entertainment"} className="nav-link" to="/entertainment"> Entertainment</Link></li>
+                                <li className="nav-item"><Link className="nav-link " key={"health"} className="nav-link" to="/health"> Health</Link></li>
+                                <li className="nav-item"><Link className="nav-link " key={"science"} className="nav-link" to="/science"> Science</Link></li>
+                                <li className="nav-item"><Link className="nav-link " key={"sports"} className="nav-link" to="/sports"> Sports</Link></li>
+                                <li className="nav-item"><Link className="nav-link " key={"technology"} className="nav-link" to="/technology"> Technology</Link></li>
                             
                             
 
--- onlinereact2/src/components/newItem.js
+++ onlinereact2/src/components/newItem.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react'
 
 export default class NewItem extends Component {
     render() {
-        let {title,description,imageUrl,newsUrl,author,date,new1}=this.props;
+        let { title, description, imageUrl, newsUrl, author, date, new1 } = this.props;
         return (
             <div>
-                <div className="card" >
-                    
-                    <img src={imageUrl} className="card-img-top"  alt="..."  />
+                <div className="card shadow p-3 mb-5 bg-white rounded" >
+
+                    <img src={imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                    <h5 className="card-title" >{title}... <span class=" position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{
-                        left: "50%" ,zIndex:"1"
+                        <h5 className="card-title" >{title}... <span class=" position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{
+                            left: "50%", zIndex: "1"
                         }}>
-    {new1}
-    <span class="visually-hidden">unread messages</span>
-  </span></h5>
-                    <p className="card-text">{description}...</p>
-                    <a href={newsUrl} target="_blank"className="btn btn-sm btn-dark">Expand</a>
-                    <hr/>
-                    <p class="card-text"><small class="text-muted">By {author?author:"unknown"} on {new Date(date).toGMTString()}</small></p>
+                            {new1}
+                            <span class="visually-hidden">unread messages</span>
+                        </span></h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Expand</a>
+                        <hr />
+                        <p class="card-text"><small class="text-muted">By {author ? author : "unknown"} on {new Date(date).toGMTString()}</small></p>
 
 
                     </div>
