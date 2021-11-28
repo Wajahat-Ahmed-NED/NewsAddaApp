940ee2fccb2455c5438f470790bda2afdf56f7df
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-11-28 19:25:23 +0500

    almost completed

--- onlinereact2/src/components/news.js
+++ onlinereact2/src/components/news.js
@@ -14,7 +14,7 @@ export default function News(props) {
     const [disable, setDisable] = useState(true)
     const [totalResult, setTotalResult] = useState(0)
     const [loading, setLoading] = useState(false)
-    
+
 
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -22,14 +22,14 @@ export default function News(props) {
     document.title = `${capitalizeFirstLetter(props.category)}-NewsAdda`
 
     const getData = () => {
-        props.setProgress(10)
+        props.setProgress(75)
         const api = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=5f8ff819ed22468fb9045d4069e3c75d&pageSize=${props.pageSize}`;
         setLoading(true)
         axios.get(api).then((res) => {
-            props.setProgress(40)
+            props.setProgress(80)
             console.log(res.data.totalResults)
             setTotalResult(res.data.totalResults)
-            props.setProgress(75)
+            props.setProgress(90)
             console.log(res.data.articles[0]);
             // console.log(res.data.articles[i])
             // articles.push(res.data.articles[i])
@@ -47,45 +47,53 @@ export default function News(props) {
     }, [])
 
     const handlePrevious = () => {
+        props.setProgress(75)
         console.log("Previous")
         setLoading(true)
         const api = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=5f8ff819ed22468fb9045d4069e3c75d&page=${page - 1}&pageSize=${props.pageSize}`;
         axios.get(api).then((res) => {
+            props.setProgress(80)
             console.log(res.data.articles[0]);
             // console.log(res.data.articles[i])
             // articles.push(res.data.articles[i])
             let a = res.data.articles
             setArticles(a)
+            props.setProgress(90)
             // console.log(articles)
             setPage(page - 1)
             setLoading(false)
+            props.setProgress(100)
         }).catch((err) => {
             console.log("Following error occured " + err)
         })
     }
     const handleNext = () => {
+        props.setProgress(75)
         console.log("Next")
         setLoading(true)
         const api = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=5f8ff819ed22468fb9045d4069e3c75d&page=${page + 1}&pageSize=${props.pageSize}`;
         axios.get(api).then((res) => {
             console.log(res.data.articles[0]);
+            props.setProgress(80)
             // console.log(res.data.articles[i])
             // articles.push(res.data.articles[i])
             let a = res.data.articles
             setArticles(a)
+            props.setProgress(90)
             // console.log(articles)
             setPage(page + 1)
             setDisable(false)
             setLoading(false)
+            props.setProgress(100)
         }).catch((err) => {
             console.log("Following error occured " + err)
         })
     }
     return (
         <div>
-            
+
             <div className="container" >
-                <h1 className="my-3 text-center">NewsAdda-Latest {capitalizeFirstLetter(props.category)} News </h1>{
+                <h1 className="mb-5 text-center" style={{ marginTop: '82px' }}>NewsAdda-Latest {capitalizeFirstLetter(props.category)} News </h1>{
                     loading && <Spinner />
                 }
 
